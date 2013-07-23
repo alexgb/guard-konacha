b35afe68891a84aead0a7704959391c72fef174a
Alex Gibbons <alex.gibbons@gmail.com>
2013-07-22 22:46:24 -0700

    Clean up formatting of pending and failing results.

--- lib/guard/konacha/formatter.rb
+++ lib/guard/konacha/formatter.rb
@@ -33,24 +33,28 @@ module Guard
 
       def write_summary
         io.puts ""
-        write_failed_examples
-        write_pending_examples
+        io.puts [
+          failed_examples_message,
+          pending_examples_message
+        ].reject(&:empty?).join("\n\n")
         io.puts ""
         io.puts summary_line
       end
 
-      def write_failed_examples
-        examples = failed_examples
-        unless examples.empty?
-          io.puts examples.map { |e| failure_message(e) }.join("\n\n")
-        end
+      def failed_examples_message
+        failed_examples.map { |e| failure_message(e) }.join("\n\n")
       end
 
-      def write_pending_examples
-        examples = pending_examples
-        unless examples.empty?
-          io.puts examples.map { |e| pending_message(e) }.join("\n\n")
-        end
+      def failure_message(example)
+        "  \xE2\x9C\x96 ".red + "#{example.metadata['path']}\n" + super
+      end
+
+      def pending_examples_message
+        pending_examples.map { |e| pending_message(e) }.join("\n\n")
+      end
+
+      def pending_message(example)
+        "  \xE2\x97\x8B ".yellow + "#{example.metadata['path']}\n" + super
       end
 
     end
