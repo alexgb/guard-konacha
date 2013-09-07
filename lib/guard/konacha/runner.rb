84490caf341d3a5432a65c97be4b8682e6d88e96
Jeremy Mickelson <Jeremy.Mickelon@gmail.com>
2013-09-06 19:23:20 -0600

    JRM only print a report if we ran at least one file

--- lib/guard/konacha/runner.rb
+++ lib/guard/konacha/runner.rb
@@ -36,16 +36,20 @@ module Guard
 
       def run(paths = [''])
         formatter.reset
+        ran_one = false
 
         paths.each do |path|
           file_path = konacha_path(path)
           if File.exist?(file_path)
             runner.run file_path
+            ran_one = true
           end
         end
 
-        formatter.write_summary
-        notify
+        if ran_one
+          formatter.write_summary
+          notify
+        end
       rescue => e
         UI.error(e)
       end
